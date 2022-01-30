20835dd84eb5abe79130fed196899cfc75b780ca
kevin <kg1941a@gmail.com>
2022-01-30 17:05:58 -0500

    adds MovieGenre and Genre models. Also adds associations.

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,4 +1,4 @@
 class MovieSerializer < ActiveModel::Serializer
-  attributes :id, :title, :year, :plot, :director, :english
+  attributes :id, :title, :year, :plot, :director, :english, :genre_names
   has_many :actors
 end
